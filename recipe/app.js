97d2bb0ad65b24caca2ee874a910ff152fe50a84
Aigerim Azamatova <moongerome@gmail.com>
2023-11-24 11:08:46 +0600

    function

---
+++ recipe/app.js
@@ -0,0 +1,31 @@
+const apiUrl = "http://localhost:3000";
+
+function loadRecipes() {
+  // Implement this function to fetch and display recipes
+}
+
+function displayRecipes(recipes) {
+  // Implement this function to display recipes in the HTML
+}
+
+function addRecipe() {
+  // Implement this function to show a form for adding a new recipe
+}
+
+function editRecipe(id) {
+  // Implement this function to show a form for editing an existing recipe
+}
+
+function submitRecipe(event) {
+  // Implement this function to handle the submission of a new recipe
+}
+
+function submitEditedRecipe(event, id) {
+  // Implement this function to handle the submission of an edited recipe
+}
+
+function resetRecipeForm() {
+  // Implement this function to reset the recipe form
+}
+
+// Implement any additional functions or modifications as needed
