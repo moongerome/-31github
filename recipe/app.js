60cf48882a501cc8720ce5cf4f50dd288b7da8ea
Aigerim Azamatova <moongerome@gmail.com>
2023-11-24 11:18:18 +0600

    changes in app js

--- recipe/app.js
+++ recipe/app.js
@@ -1,11 +1,28 @@
 const apiUrl = "http://localhost:3000";
 
+// app.js
+
 function loadRecipes() {
-  // Implement this function to fetch and display recipes
+  fetch(`${apiUrl}/recipes`)
+    .then((response) => response.json())
+    .then((recipes) => displayRecipes(recipes))
+    .catch((error) => console.error("Error loading recipes:", error));
 }
 
 function displayRecipes(recipes) {
-  // Implement this function to display recipes in the HTML
+  const recipeList = document.getElementById("recipeList");
+  recipeList.innerHTML = "";
+
+  recipes.forEach((recipe) => {
+    const recipeItem = document.createElement("div");
+    recipeItem.innerHTML = `
+        <h3>${recipe.title}</h3>
+        <p>${recipe.ingredients}</p>
+        <p>${recipe.instructions}</p>
+        <button onclick="editRecipe('${recipe.id}')">Edit</button>
+      `;
+    recipeList.appendChild(recipeItem);
+  });
 }
 
 function addRecipe() {
@@ -16,12 +33,62 @@ function editRecipe(id) {
   // Implement this function to show a form for editing an existing recipe
 }
 
+// app.js
+
 function submitRecipe(event) {
-  // Implement this function to handle the submission of a new recipe
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const ingredients = document.getElementById("ingredients").value;
+  const instructions = document.getElementById("instructions").value;
+
+  const newRecipe = {
+    title,
+    ingredients,
+    instructions,
+  };
+
+  fetch(`${apiUrl}/recipes`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newRecipe),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      loadRecipes();
+      resetRecipeForm();
+    })
+    .catch((error) => console.error("Error adding recipe:", error));
 }
 
 function submitEditedRecipe(event, id) {
-  // Implement this function to handle the submission of an edited recipe
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const ingredients = document.getElementById("ingredients").value;
+  const instructions = document.getElementById("instructions").value;
+
+  const editedRecipe = {
+    title,
+    ingredients,
+    instructions,
+  };
+
+  fetch(`${apiUrl}/recipes/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(editedRecipe),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      loadRecipes();
+      resetRecipeForm();
+    })
+    .catch((error) => console.error("Error editing recipe:", error));
 }
 
 function resetRecipeForm() {
