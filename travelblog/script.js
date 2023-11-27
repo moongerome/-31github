c6e1f69de6ba86e053675f18892d679376a2b881
Aigerim Azamatova <moongerome@gmail.com>
2023-11-27 10:45:40 +0600

    add post

--- travelblog/script.js
+++ travelblog/script.js
@@ -1,30 +1,26 @@
 function addPost() {
   const title = document.getElementById("title").value;
   const content = document.getElementById("content").value;
-  function addPost() {
-    const title = document.getElementById("title").value;
-    const content = document.getElementById("content").value;
 
-    if (title && content) {
-      const postContainer = document.createElement("div");
-      postContainer.classList.add("post");
+  if (title && content) {
+    const postContainer = document.createElement("div");
+    postContainer.classList.add("post");
 
-      const postTitle = document.createElement("h2");
-      postTitle.textContent = title;
+    const postTitle = document.createElement("h2");
+    postTitle.textContent = title;
 
-      const postContent = document.createElement("p");
-      postContent.textContent = content;
+    const postContent = document.createElement("p");
+    postContent.textContent = content;
 
-      postContainer.appendChild(postTitle);
-      postContainer.appendChild(postContent);
+    postContainer.appendChild(postTitle);
+    postContainer.appendChild(postContent);
 
-      document.querySelector("main").appendChild(postContainer);
+    document.querySelector("main").appendChild(postContainer);
 
-      // Clear the form fields
-      document.getElementById("title").value = "";
-      document.getElementById("content").value = "";
-    } else {
-      alert("Please fill in both title and content fields.");
-    }
+    // Clear the form fields
+    document.getElementById("title").value = "";
+    document.getElementById("content").value = "";
+  } else {
+    alert("Please fill in both title and content fields.");
   }
 }
