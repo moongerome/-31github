23cd1f8d0229c21909bd82984811d66332af3a95
Aigerim Azamatova <moongerome@gmail.com>
2023-11-27 10:39:29 +0600

    value

--- travelblog/script.js
+++ travelblog/script.js
@@ -1,4 +1,28 @@
 function addPost() {
   const title = document.getElementById("title").value;
   const content = document.getElementById("content").value;
+  function addPost() {
+    const title = document.getElementById("title").value;
+    const content = document.getElementById("content").value;
+
+    if (title && content) {
+      const postContainer = document.createElement("div");
+      postContainer.classList.add("post");
+
+      const postTitle = document.createElement("h2");
+      postTitle.textContent = title;
+
+      const postContent = document.createElement("p");
+      postContent.textContent = content;
+
+      postContainer.appendChild(postTitle);
+      postContainer.appendChild(postContent);
+
+      // Clear the form fields
+      document.getElementById("title").value = "";
+      document.getElementById("content").value = "";
+    } else {
+      alert("Please fill in both title and content fields.");
+    }
+  }
 }
