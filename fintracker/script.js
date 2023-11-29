3aeda6056e1976f818d910976ac24c698bd1ff51
Aigerim Azamatova <moongerome@gmail.com>
2023-11-29 11:48:29 +0600

    additional function

--- fintracker/script.js
+++ fintracker/script.js
@@ -70,3 +70,82 @@ function updateSummary() {
   totalExpensesSpan.textContent = totalExpenses.toFixed(2);
   balanceSpan.textContent = balance.toFixed(2);
 }
+
+// ... (unchanged code)
+
+function addTransaction() {
+  // ... (unchanged code)
+
+  // Update the category dropdown with unique categories
+  const filterCategoryDropdown = document.getElementById("filter-category");
+  if (
+    !filterCategoryDropdown.innerHTML.includes(
+      `<option value="${category}">${category}</option>`
+    )
+  ) {
+    filterCategoryDropdown.innerHTML += `<option value="${category}">${category}</option>`;
+  }
+
+  // Clear input fields
+  descriptionInput.value = "";
+  amountInput.value = "";
+  categoryInput.value = "";
+
+  updateChart();
+}
+
+function filterTransactionsByCategory() {
+  const filterCategoryDropdown = document.getElementById("filter-category");
+  const selectedCategory = filterCategoryDropdown.value;
+
+  if (selectedCategory === "all") {
+    updateTransactionList(transactions);
+  } else {
+    const filteredTransactions = transactions.filter(
+      (transaction) => transaction.category === selectedCategory
+    );
+    updateTransactionList(filteredTransactions);
+  }
+}
+
+function updateChart() {
+  const expenseChartCanvas = document.getElementById("expense-chart");
+  const expenseChartContext = expenseChartCanvas.getContext("2d");
+
+  const expenseData = {};
+  transactions
+    .filter((transaction) => transaction.amount < 0)
+    .forEach((transaction) => {
+      if (expenseData[transaction.category]) {
+        expenseData[transaction.category] += Math.abs(transaction.amount);
+      } else {
+        expenseData[transaction.category] = Math.abs(transaction.amount);
+      }
+    });
+
+  const labels = Object.keys(expenseData);
+  const data = Object.values(expenseData);
+
+  // Update the chart using Chart.js
+  new Chart(expenseChartContext, {
+    type: "pie",
+    data: {
+      labels,
+      datasets: [
+        {
+          data,
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#8e5ea2",
+            "#3cba9f",
+          ],
+        },
+      ],
+    },
+  });
+}
+
+// Initial chart update
+updateChart();
