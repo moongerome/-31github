9da205da7cfeb64d87d23351db9194708bad6397
Aigerim Azamatova <moongerome@gmail.com>
2023-12-06 10:32:32 +0600

    deletions

--- memorygame/script.js
+++ memorygame/script.js
@@ -1,20 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cards = ["A", "B", "C", "D", "A", "B", "C", "D"];
-  let flippedCards = [];
-  let matchedCards = [];
 
-  const memoryGame = document.querySelector(".memory-game");
-
-  // Shuffle the cards
-  cards.sort(() => Math.random() - 0.5);
-
-  // Create cards dynamically
-  cards.forEach((card, index) => {
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
-    cardElement.setAttribute("data-index", index);
-    cardElement.textContent = card;
-    cardElement.addEventListener("click", flipCard);
-    memoryGame.appendChild(cardElement);
-  });
-});
+  
+  
