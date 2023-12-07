50edf412da200c7cb90705e8d4a040bb2afb83b4
Aigerim Azamatova <moongerome@gmail.com>
2023-12-07 10:39:06 +0600

    message

--- draw/script.js
+++ draw/script.js
@@ -6,4 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
   // Set canvas size
   canvas.width = window.innerWidth - 20;
   canvas.height = window.innerHeight - 20;
+
+  // Set initial drawing style
+  context.lineWidth = 5;
+  context.lineCap = "round";
+  context.strokeStyle = "#000";
 });
