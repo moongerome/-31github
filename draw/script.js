8c74cff61c3b14ce6ffa3a36bc666fabc4c130d4
Aigerim Azamatova <moongerome@gmail.com>
2023-12-07 10:38:44 +0600

    message

--- draw/script.js
+++ draw/script.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("drawingCanvas");
   const context = canvas.getContext("2d");
   let isDrawing = false;
+
+  // Set canvas size
+  canvas.width = window.innerWidth - 20;
+  canvas.height = window.innerHeight - 20;
 });
